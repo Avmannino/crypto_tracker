fc2ab7ec17ab807323962a8eff648ac83dd5e59e
AdamMannino <amannino92@gmail.com>
2025-01-11 11:05:11 -0500

    Minor layout changes

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,8 @@
 'use client';
-
-import Dashboard from '../../components/Dashboard';  
+import React from "react";
+import ReactDOM from "react-dom";
+import Dashboard from '@/components/Dashboard';
+// import Dashboard from '../../components/Dashboard';  
 
 export default function DashboardPage() {
   return (
